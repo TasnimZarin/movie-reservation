83a0fc890f9a19f9280f060f8778cdb64138881b
Zarin Tasnim <zarintasnim587@gmail.com>
2026-03-12 00:47:54 -0400

    fix CORS for Vercel

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.database import engine
-from app import models
+from app.database import engine, Base
 from app.routers import auth, movies, theaters, seats, showtimes, bookings
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Movie Reservation System")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://movie-reservation-five.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
