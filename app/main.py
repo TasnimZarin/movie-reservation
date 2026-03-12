eeb5bd44311fad85ca938d08fd344ea44875e945
Zarin Tasnim <zarintasnim587@gmail.com>
2026-03-11 23:14:27 -0400

    update API URL to Render

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
-from app.database import engine
-from app import models
+from fastapi.middleware.cors import CORSMiddleware
+from app.database import engine, Base
 from app.routers import auth, movies, theaters, seats, showtimes, bookings
 
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="Movie Reservation System")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(movies.router)
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API = 'http://127.0.0.1:8000';
+// const API = 'http://127.0.0.1:8000';
+const API = 'https://movie-reservation-po09.onrender.com';
 
 function Login(props) {
   const [email, setEmail] = useState('');
