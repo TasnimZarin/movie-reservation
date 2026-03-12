da16ba6ffaf96760d4f43a99e1d6b6ed3fa337d3
Zarin Tasnim <zarintasnim587@gmail.com>
2026-03-11 23:03:43 -0400

    auto-create tables on startup

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine
 from app import models
 from app.routers import auth, movies, theaters, seats, showtimes, bookings
 
 app = FastAPI(title="Movie Reservation System")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(movies.router)
